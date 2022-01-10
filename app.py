35361b6f7f6217d56868e30d4f1198086918c144
Ibraheem Alyan <ibraheemalayan@gmail.com>
2022-01-10 20:50:57 +0200

    seperated file read in a function

--- app.py
+++ app.py
@@ -1,8 +1,42 @@
 from typing import List
 from utils import get_hours_from_course_id
 from models import Student, SemesterRecord
+from os import path
 
 students: List[Student] = []
+
+def read_file(id: int) -> Student:
+    
+    semesters = {}
+        
+    with open( path.join("db", str(id) + ".txt") , "r") as f:
+        
+        for line in f.readlines():
+            
+            year_sem = line.split(";")[0].strip()
+            
+            recs = line.split(";")[1].split(",")
+            
+            sem_dict = {}
+            
+            
+            for r in recs:
+                splitted = r.strip().split(" ")
+                
+                course_id = splitted[0]
+                mark = float(splitted[1])
+                
+                hours = get_hours_from_course_id(course_id)
+                
+                sem_dict[course_id] = (mark, hours)
+                
+                semesters[year_sem] = SemesterRecord(year_sem=year_sem, courses_dict=sem_dict)
+                
+            print("student_id : " + str(id))
+            print("sem : " + str(sem_dict))
+            
+    return Student(id, semesters=semesters)
+    
 
 def read_files():
 
@@ -11,40 +45,15 @@ def read_files():
     filenames = next(walk("db"), (None, None, []))[2]
     
     for filename in filenames:
+        
         id = Student.is_valid_id(filename.split(".")[0])
         
         if id is None:
             continue
         
-        semesters = {}
+        students.append(read_file(id))
         
-        with open( path.join("db", filename) , "r") as f:
-            
-            for line in f.readlines():
-                
-                year_sem = line.split(";")[0].strip()
-                
-                recs = line.split(";")[1].split(",")
-                
-                sem_dict = {}
-                
-                
-                for r in recs:
-                    splitted = r.strip().split(" ")
-                    
-                    course_id = splitted[0]
-                    mark = float(splitted[1])
-                    
-                    hours = get_hours_from_course_id(course_id)
-                    
-                    sem_dict[course_id] = (mark, hours)
-                    
-                    semesters[year_sem] = SemesterRecord(year_sem=year_sem, courses_dict=sem_dict)
-                    
-                print("student_id : " + str(id))
-                print("sem : " + str(sem_dict))
-                
-        students.append(Student(id, semesters=semesters))
+        
         
     for st in students:
         print(st)
